fe0dd3683daa6c5f1c674daecb865bb2af3b149a
David Shaw <mx496@nyu.edu>
2020-04-29 02:59:28 -0400

    Update model.py

--- model.py
+++ model.py
@@ -20,10 +20,33 @@ from glob import glob
 from PIL import Image
 
 # model
-...
+class Net(nn.Module):
+    def __init__(self, ...):
+        ...
+    def forward(self, x):
+        ...
 
 # dataset
-...
+class GTSRB(data.Dataset):
+    def __init__(self,path,transform=None):
+        self.path = path
+        self.data = []
+        self.transform = transform
+        class_folders_paths = sorted(glob(os.path.join("{}/Final_Training/Images".format(self.path), "*")))
+        for folders_paths in class_folders_paths:
+            folder_img_paths = sorted(glob(os.path.join(folders_paths, "*.ppm")))
+            self.data.extend([(_,int(folders_paths.split("/")[-1])) for _ in folder_img_paths])
+
+    def __getitem__(self, i):
+        img_path,label = self.data[i]
+        im = Image.open(img_path)
+        if self.transform: im = self.transform(im)
+        return im,label
+
+    def __len__(self): return len(self.data)
+
+def get_transform():
+    return T.Compose([T.Resize((48,48)), T.ToTensor(), T.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.247, 0.243, 0.261])])
 
 # training and evaluation pipeline
 def train_one_epoch(model, train_loader, optimizer, criterion, device):
@@ -62,17 +85,18 @@ def save_model(model, dest):
 def main():
     torch.manual_seed(0)
 
+    training_data_path = ...
     NUM_EPOCHS = 50
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     optimizer = ... # optimizer
     criterion = ... # loss
-    model = ... # instantiate model
+    model = Net(...) # instantiate model
     dest = ... # [folder]/[model].pth
 
     model.to(device)
     
-    dataset = ...
+    dataset = GTSRB('./GTSRB',transform=get_transform())
     train_loader = DataLoader(dataset, batch_size=1, shuffle=False, sampler=None,
            batch_sampler=None, num_workers=0, collate_fn=None,
            pin_memory=False, drop_last=False, timeout=0,
@@ -88,7 +112,6 @@ def main():
 
     save_model(model, dest)
     plot_accuracy(accuracies)
-
 
 # test
 # plot_accuracy(np.random.normal(1,2,50))
